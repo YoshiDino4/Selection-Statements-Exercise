ea3f901fc1fb9fbd7f7a81a3e7dfa6089854dde2
Yash Bajaj <yash@bajajmd.net>
2024-07-11 12:05:05 -0400

    Finished exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Exercise 1 rng
+            var r = new Random();
+            var fav = r.Next(1, 1000);
+            
+            //User input
+            Console.WriteLine("I'm thinking of a number 1 - 1000, can you guess what it is?");
+            var userInput = int.Parse(Console.ReadLine());
+
+            bool guess = false;
+
+            while (guess == false)
+            {
+                if (userInput > fav)
+                {
+                    Console.WriteLine("Too high, guess again: ");
+                    userInput = int.Parse(Console.ReadLine());
+                }
+                else if (userInput < fav)
+                {
+                    Console.WriteLine("Too low, guess again: ");
+                    userInput = int.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    Console.WriteLine("Congratulations, you got it!");
+                    guess = true;
+                }
+            }
         }
     }
 }
